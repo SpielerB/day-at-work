b62550bc074cfa4f48a30fdc33a02a99f1867baf
SpielerB <32329914+SpielerB@users.noreply.github.com>
2019-11-26 14:50:20 +0100

    Added VR Settings and changed build target to android Added the GoogleVR assets Added Basic movement system for cardboard VR Removed FPSController Added VRController Added camera control to the arrow keys for usage in the editor Added textures

---
+++ Assets/Scripts/MovementNode.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementNode : MonoBehaviour
+{
+
+    public Image interactionTimerIndicator;
+    public PlayerController player;
+    [Tooltip("How many seconds until the player moves to this MovementPoint")]
+    public float movementDelay = 2f;
+    
+    private float step;
+    private float timer;
+    private bool lookingAt;
+
+    private void Start()
+    {
+        step = 1f / movementDelay;
+    }
+
+    private void Update()
+    {
+        if (!lookingAt || interactionTimerIndicator == null) return;
+        interactionTimerIndicator.fillAmount += step * Time.deltaTime;
+        if (timer <= movementDelay)
+        {
+            timer += Time.deltaTime;
+            return;
+        }
+        player.MoveTo(this);
+        SetLookingAt(false);
+        interactionTimerIndicator.fillAmount = 0;
+    }
+
+    public void SetLookingAt(bool look)
+    {
+        lookingAt = look;
+        timer = 0;
+        if (interactionTimerIndicator != null)
+        {
+            interactionTimerIndicator.fillAmount = 0;
+        }
+    }
+
+}
