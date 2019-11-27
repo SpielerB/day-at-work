db803af76b76309c2bbd0899865eb0c8a3ffcf1d
SpielerB <32329914+SpielerB@users.noreply.github.com>
2019-11-27 14:56:46 +0100

    Add Window prototype to the Computer

---
+++ Assets/Scripts/ComputerWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerWindow : MonoBehaviour
+{
+
+    public virtual bool CanClose()
+    {
+        return true;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+}
