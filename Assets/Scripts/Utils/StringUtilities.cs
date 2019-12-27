445aad35b81485752feb6fc6f9c0b9c2ad38c0a5
SpielerB <32329914+SpielerB@users.noreply.github.com>
2019-12-27 17:02:04 +0100

    Updated the way preview strings are generated to avoid wrapping/overflow issues

--- Assets/Scripts/Utils/StringUtilities.cs
+++ Assets/Scripts/Utils/StringUtilities.cs
@@ -5,7 +5,7 @@
 
         public static string Preview(this string text)
         {
-            var preview = text.Replace('\n', ' ').Replace('\t', ' ');
+            var preview = text.Replace('\t', ' ').Split('\n')[0];
             return preview.Length > 20 ? preview.Substring(0, 20) : preview;
         }
 
